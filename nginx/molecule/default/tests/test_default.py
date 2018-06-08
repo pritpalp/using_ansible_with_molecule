9412ef1899d7eb86bf4e2ebd5b5faa5f6c37148b
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-06-08 16:26:49 +0100

    Add in a file copy and check

--- nginx/molecule/default/tests/test_default.py
+++ nginx/molecule/default/tests/test_default.py
@@ -16,6 +16,18 @@ def test_hosts_file(host):
 
 def test_nginx(host):
     # is the nginx package installed
-    assert host.package("nginx").is_installed
+    assert host.package('nginx').is_installed
     # is it running
-    assert host.service("nginx").is_running
+    assert host.service('nginx').is_running
+
+
+def test_nginx_index(host):
+    f = host.file('/usr/share/nginx/html/index.html')
+    # Does the index.html file exist in the correct folder
+    assert f.exists
+
+
+def test_index_content(host):
+    f = host.file('/usr/share/nginx/html/index.html')
+    # Is it the file we put there?
+    assert f.contains('Hello World!')
